19322d6bbcc83a0c006037e570ab9681c7d9d2a3
y-shi23 <167186849+y-shi23@users.noreply.github.com>
2025-01-06 23:51:28 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -35,6 +35,7 @@ let audio = null
 document.addEventListener("DOMContentLoaded", () => {
   audio = new Audio("music/bgMusic.mp3")
   audio.preload = "auto"
+  audio.play()//初始状态播放音频
 })
 
 const playPauseButton = document.getElementById('playPauseButton')
